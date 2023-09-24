5ff07f72930966090f92e502e3b269747028c0db
mikiasmh <mikias.mohamed@sada.com>
2023-09-24 08:10:19 -0400

    Refactor and add requirements.txt

--- app.py
+++ app.py
@@ -2,41 +2,51 @@ from langchain.document_loaders import RSSFeedLoader
 import streamlit as st
 from langchain.llms import VertexAI
 from google.oauth2.service_account import Credentials
+import validators
+import logging
 
-# Load the credentials from Streamlit secrets
-google_secrets = st.secrets["google"]
-credentials = Credentials.from_service_account_info(google_secrets)
-
-llm = VertexAI(model_name='text-bison@001', credentials=credentials)
-
-st.header("RSS Feed Summaries")
-
-user_input_url = st.text_input("Enter the RSS feed URL to get summaries of articles:", "https://www.bleepingcomputer.com/feed/")
-st.markdown('')
-
-params = {
-    'temperature': 0.7,
-    'top_p': 0.9,
-}
-
-if user_input_url:
-    urls = [user_input_url]
-    
-    with st.spinner("Loading content..."):
-        try:
+def load_credentials():
+    try:
+        google_secrets = st.secrets["google"]
+        credentials = Credentials.from_service_account_info(google_secrets)
+        return credentials
+    except Exception as e:
+        logging.error(f"Error loading credentials, exception: {e}")
+        st.error(f"Error loading credentials, exception: {e}")
+        st.stop()
         
-            loader = RSSFeedLoader(urls=urls)
+def load_and_process_data(url, llm, params):
+    content = ""
+    with st.spinner("Loading content..."):
+    
+        try:
+            loader = RSSFeedLoader(urls=[url])
             data = loader.load()
-
-            content = ""
-
             for item in data:
                 content += f"### {item.metadata['title']}\n"
                 prompt = f"Summarize the following article in about 3 to 4 lines:\n{item.page_content}"
-                content += f" {llm(prompt, **params)}\n"                                    
-            
-            st.markdown(content)
-        
-        
+                content += f" {llm(prompt, **params)}\n"
         except Exception as e:
-            st.error(f"Error fetching {user_input_url}, exception: {e}")
+                st.error(f"Error fetching {url}, Pleae try again later.")        
+        return content
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    credentials = load_credentials()
+    
+    llm = VertexAI(model_name='text-bison@001', credentials=credentials)
+    params = { 'temperature': 0.7, 'top_p': 0.9 }
+
+    st.header("Gen AI RSS Feed Summaries App")
+
+    user_input_url = st.text_input("Enter the RSS feed URL to get summaries of articles:", "https://www.bleepingcomputer.com/feed/")
+    st.markdown('')
+    
+    if validators.url(user_input_url):
+        content = load_and_process_data(user_input_url, llm, params)
+        st.markdown(content)
+    else:
+        st.error(f"Invalid entry. Please enter a valid RSS feed URL.")
+if __name__ == "__main__":
+    main()
